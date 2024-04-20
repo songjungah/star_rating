0237f8e74b452035e8e7fa50a4cc64e3d9510bb5
songjungah <1104wjddk@naver.com>
2024-04-20 10:38:50 +0900

    240420 star-rating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+// starRating 컴포넌트를 import 한다
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StarRating/>
     </div>
   );
 }
